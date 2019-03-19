a3281b4f14850c2d4e290954eca971f0f93d1c49
AliAbdulle <alidhobey@gmail.com>
2019-03-19 15:20:04 -0500

    Printed all the agent name

---
+++ src/scripts/main.js
@@ -0,0 +1,18 @@
+//console.log("businesses")
+const outEl = document.querySelector("#output")
+outEl.innerHTML = "<h1>Active Businesses</h1>"
+
+/*
+    Using map(), you extract the purchasing agent object
+    from each business and store it in a new array
+*/
+const agents = businesses.map(business => {
+  return business.purchasingAgent
+})
+
+console.table(agents)
+
+agents.forEach(agent => {
+outEl.innerHTML += `<h2>${agent.nameFirst} ${agent.nameLast}</h2>`;
+outEl.innerHTML += "<hr/>";
+});
